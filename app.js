94dc41d53a269b2a41bc343dd52fa84ceffd5b59
= <jy7123943@gmail.com>
2019-11-02 13:01:19 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_ATLAS_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection-error:'));
+
+db.once('open', function () {
+  console.log('Bugcide DB connected');
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+module.exports = app;
